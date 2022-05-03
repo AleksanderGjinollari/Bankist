b45aadfed2bc777f6fc9d3df9a3d60e8e00549b8
Aleksander Gjinollari <aleksandergjinollari@hotmail.com>
2022-05-03 18:13:43 +0200

    Revealing elements on scroll

--- script.js
+++ script.js
@@ -273,6 +273,24 @@ const stickyNav = function (entries) {
 const headerObserver = new IntersectionObserver(stickyNav, {
   root: null,
   threshold: 0,
-  rootMargin: `${navHeight}`,
+  rootMargin: `${navHeight}px`,
 });
 headerObserver.observe(header);
+
+// Reveal sections
+
+const revealSection = function (entries, observer) {
+  const [entry] = entries;
+  console.log(entry);
+  if (!entry.isIntersecting) return;
+  entry.target.classList.remove('section--hidden');
+  observer.unobserve(entry.target);
+};
+const sectionObserver = new IntersectionObserver(revealSection, {
+  root: null,
+  threshold: 0.15,
+});
+allSections.forEach(function (section) {
+  sectionObserver.observe(section);
+  section.classList.add('section--hidden');
+});
