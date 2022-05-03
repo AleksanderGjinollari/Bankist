32391ed0582bd67299149eea33afef74e8a2ccd0
Aleksander Gjinollari <aleksandergjinollari@hotmail.com>
2022-05-03 20:24:27 +0200

    defer and async

--- script.js
+++ script.js
@@ -385,3 +385,17 @@ const sliderFunction = function () {
   });
 };
 sliderFunction();
+
+// document.addEventListener('DOMContentLoaded', function (e) {
+//   console.log(`HTML parsed and DOM tree built!`, e);
+// });
+
+// window.addEventListener('load', function (e) {
+//   console.log('fully loaded', e);
+// });
+
+// window.addEventListener('beforeunload', function (e) {
+//   e.preventDefault();
+//   console.log(e);
+//   e.returnValue = '';
+// });
